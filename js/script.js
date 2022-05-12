3b17de57f2f35cadc20cb514e180ed32777deb5a
vyankevich <vit.yankevich@gmail.com>
2022-05-12 16:15:49 +0300

    updates

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@ $(document).ready(function () {
         $(this).toggleClass('active').next().slideToggle(300);
     });
 });
+
+new Swiper('.image-slider', {
+    spaceBetween: 10,
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    },
+    loop: true,
+    breakpoints: {
+        700: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        1300: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+        }
+    },
+});
+
