b80d8690c55a6f2a9ff709f39560e061ce4998fb
vyankevich <vit.yankevich@gmail.com>
2022-05-25 16:34:38 +0300

    Added more js
    add "go to top" button
    add func that gets info from "contact us" form

--- js/script.js
+++ js/script.js
@@ -48,3 +48,35 @@ let elements = document.querySelectorAll('.animation');
 for (let elm of elements) {
     observer.observe(elm);
 };
+
+var mybutton = document.getElementById("top__button");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 950 || document.documentElement.scrollTop > 950) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+const form = document.getElementById('form__items');
+form.addEventListener('submit', getFormValue);
+function getFormValue(event) {
+    event.preventDefault();
+    const name = form.querySelector('[name="name"]'),
+        email = form.querySelector('[name="email"]'),
+        message = form.querySelector('[name="message"]');
+    const data = {
+        name: name.value,
+        email: email.value,
+        message: message.value,
+    }
+    console.log(data);
+};
