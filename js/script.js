5602ce1ab304e7797357c418a964833eee92b360
vyankevich <vit.yankevich@gmail.com>
2022-08-01 23:34:43 +0300

    adaptive for max-width767px (not 100% done)

--- js/script.js
+++ js/script.js
@@ -6,33 +6,49 @@ $(document).ready(function () {
 
 new Swiper('.image-slider', {
     spaceBetween: 10,
-    navigation: {
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next'
-    },
-    loop: true,
-    slidesPerView: 5
-    // breakpoints: {
-    //     500: {
-    //         slidesPerView: 1,
-    //         spaceBetween: 10,
-    //     },
-    //     1000: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 10,
-    //     },
-    //     1300: {
-    //         slidesPerView: 5,
-    //         spaceBetween: 10,
-    //     }
+    // navigation: {
+    //     prevEl: '.swiper-button-prev',
+    //     nextEl: '.swiper-button-next'
     // },
+    loop: true,
+    breakpoints: {
+        500: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        700: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        1300: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+        }
+    },
 });
 
 new Swiper('.press-slider', {
     spaceBetween: 50,
-    slidesPerView: 3,
     loop: true,
-    initialSlide: 0
+    initialSlide: 0,
+    breakpoints: {
+        500: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1300: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        }
+    },
 })
 
 function onEntry(entry) {
